e1ebc89b081da00d37a089ae32325310d1d56cf8
Whitney <169212050+whitneybarr@users.noreply.github.com>
2024-08-07 22:34:59 -0500

    Create one round of game

--- script.js
+++ script.js
@@ -11,15 +11,47 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let choice = prompt('Rock, Paper or Scissors?');
-    if (choice === "rock" || choice === "Rock") {
+    let validChoice= choice.toLowerCase();
+    if (validChoice === "rock") {
         return "rock"
-    } else if (choice === "paper" || choice === "Paper") {
+    } else if (validChoice === "paper") {
         return "paper"
-    } else if (choice === "scissors" || choice === "Scissors") {
+    } else if (validChoice === "scissors") {
         return "scissors"
     } else {
         return "Please enter a valid option"
     }
 }
 
-console.log(getHumanChoice())
+let humanScore = 0;
+let computerScore = 0;
+
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    if (humanChoice === computerChoice) {
+        console.log("Tie! No winner this round.")
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        ++computerScore
+        console.log("You Lose! Paper beats Rock!")
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        ++humanScore
+        console.log("You Win! Rock beats Scissors!")
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        ++computerScore
+        console.log("You Lose! Scissors beats Paper!")
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        ++humanScore
+        console.log("You Win! Paper beats Rock!")
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        ++computerScore
+        console.log("You Lose! Rock beats Scissors!")
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        ++humanScore
+        console.log("You Win! Scissors beats Paper!")
+    } 
+}
+
+
+playRound()
