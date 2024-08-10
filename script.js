54ea30452066acf2504b25339ef42ec73502913f
Whitney <169212050+whitneybarr@users.noreply.github.com>
2024-08-09 23:23:53 -0500

    Update DOM with game events

--- script.js
+++ script.js
@@ -9,59 +9,59 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt('Rock, Paper or Scissors?');
-    let validChoice= choice.toLowerCase();
-    if (validChoice === "rock") {
-        return "rock"
-    } else if (validChoice === "paper") {
-        return "paper"
-    } else if (validChoice === "scissors") {
-        return "scissors"
-    } else {
-        console.log("Please enter a valid option")
-    }
-}
+const rpsChoices = document.querySelector("#rpsChoices")
+const results = document.querySelector("#results")
+const button = document.querySelectorAll("button")
 
 let humanScore = 0;
 let computerScore = 0;
 
-function playGame() {
+
 function playRound(humanChoice, computerChoice) {
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-    if (humanChoice === computerChoice) {
-        console.log(`Tie! No winner this round. You: ${humanScore} Computer: ${computerScore}`)
+    if (humanScore >= 5 || computerScore >= 5) {
+        for (let i = 0; 1 < button.length; i++) {
+            e.stopPropagation();
+            button.disabled = true;
+        }
+    } else if (humanChoice === computerChoice) {
+        results.textContent = `Tie! No winner this round. You: ${humanScore} Computer: ${computerScore}`
     } else if (humanChoice === "rock" && computerChoice === "paper") {
         ++computerScore
-        console.log(`You Lose! Paper beats Rock! You: ${humanScore} Computer: ${computerScore}`)
+        results.textContent = `You Lose! Paper beats Rock! You: ${humanScore} Computer: ${computerScore}`
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
         ++humanScore
-        console.log(`You Win! Rock beats Scissors! You: ${humanScore} Computer: ${computerScore}`)
+        results.textContent = `You Win! Rock beats Scissors! You: ${humanScore} Computer: ${computerScore}`
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
         ++computerScore
-        console.log(`You Lose! Scissors beats Paper! You: ${humanScore} Computer: ${computerScore}`)
+        results.textContent = `You Lose! Scissors beats Paper! You: ${humanScore} Computer: ${computerScore}`
     } else if (humanChoice === "paper" && computerChoice === "rock") {
         ++humanScore
-        console.log(`You Win! Paper beats Rock! You: ${humanScore} Computer: ${computerScore}`)
+        results.textContent = `You Win! Paper beats Rock! You: ${humanScore} Computer: ${computerScore}`
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
         ++computerScore
-        console.log(`You Lose! Rock beats Scissors! You: ${humanScore} Computer: ${computerScore}`)
+        results.textContent = `You Lose! Rock beats Scissors! You: ${humanScore} Computer: ${computerScore}`
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
         ++humanScore
-        console.log(`You Win! Scissors beats Paper! You: ${humanScore} Computer: ${computerScore}`)
+        results.textContent = `You Win! Scissors beats Paper! You: ${humanScore} Computer: ${computerScore}`
     } 
 }
-// for (let i = 0; i < 5; i++) {
-// playRound() }
-// if (humanScore > computerScore) {
-//     console.log(`You are the winner! You beat the computer by ${humanScore - computerScore} points!`)
-// } else if (humanScore < computerScore) {
-//     console.log(`You are the loser! You lost the game by ${computerScore - humanScore} points!`)
-// } else if (humanScore === computerScore) {
-//     console.log(`It's a tie! You both have ${humanScore} points!`)
-// }
-playRound()
-}
 
-playGame()
+rpsChoices.addEventListener("click", (e) => {
+    let target = e.target;
+    let humanChoice;
+    if (target.id === "rock") {
+         humanChoice = "rock"
+    } else if (target.id === "paper") {
+         humanChoice = "paper"
+    } else if (target.id === "scissors") {
+         humanChoice = "scissors"
+    }
+    playRound(humanChoice, getComputerChoice());
+    if (humanScore >= 5 || computerScore >= 5) {
+        if (humanScore > computerScore) {   
+        results.textContent = `Game Over! You Win! Your score = ${humanScore}, Computer score = ${computerScore}`
+    } else if (humanScore < computerScore) {
+        results.textContent = `Game Over! You Lose! Your score = ${humanScore}, Computer score = ${computerScore}`
+    }  
+    }
+});
